05ab3a87d5528cccb4db3193f9525a73f4a972a7
Tejas Barhate <Tjbarhate11@gmail.com>
2020-05-19 13:16:09 +0530

    Add files via upload

---
+++ ChatClient.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.net.Socket;
+
+
+public class ChatClient {
+
+    static Socket s;
+
+    static int port;
+
+    static String name;
+
+    public static void main(String[] args) {
+
+        Receivemsg1 receive = new Receivemsg1();
+        Sendmsg1 send = new Sendmsg1();
+
+        Thread receiver = new Thread(receive);
+        Thread sender = new Thread(send);
+
+        try{
+            port = Integer.parseInt(args[2]);
+            s = new Socket(args[1], port);
+            name = args[0];
+            System.out.println("Connected to: " + s.getRemoteSocketAddress());
+            System.out.println("Welcome " + name + "!");
+            System.out.println("** Note: Type 'bye' and press Enter to disconnect **");
+
+            receiver.start();
+            sender.start();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Sendmsg1 implements Runnable {
+    @Override
+    public void run() {
+        String input;
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(ChatClient.s.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            while (!(input = br.readLine()).equals("bye")){
+                out.println(input);
+            }
+            out.println("Client disconnected");
+            ChatClient.s.close();
+        } catch (IOException e) {
+            System.out.println("Disconnected");
+        }
+    }
+}
+
+
+
+class Receivemsg1 implements Runnable {
+    @Override
+    public void run() {
+        String line;
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(ChatClient.s.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            while((line = in.readLine()) != null) {
+                if(line.equals("Server disconnected")){
+                    System.out.println("> Server: bye");
+                    System.out.println(line);
+                    break;
+                }
+                System.out.println("> Server: " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Disconnected");
+        }
+    }
+}
---
+++ ChatServer.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ChatServer {
+
+    static int port;
+
+    static ServerSocket ss;
+    static Socket s;
+
+    public static void main(String[] args) {
+
+        Receivemsg receive = new Receivemsg();
+        Sendmsg send = new Sendmsg();
+
+        Thread receiver = new Thread(receive);
+        Thread sender = new Thread(send);
+
+        try{
+            port = Integer.parseInt(args[0]);
+            ss = new ServerSocket(port);
+            System.out.println("Server Started with port: " + ss.getLocalPort());
+
+            s = ss.accept();
+            System.out.println("Connection Established with client: " + s.getRemoteSocketAddress());
+            System.out.println("** Note: Type 'bye' and press Enter to disconnect **");
+
+            receiver.start();
+            sender.start();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class Sendmsg implements Runnable {
+    @Override
+    public void run() {
+        String input;
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(ChatServer.s.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            while (!(input = br.readLine()).equals("bye")){
+                out.println(input);
+            }
+            out.println("Server disconnected");
+            ChatServer.s.close();
+        } catch (IOException e) {
+            System.out.println("Disconnected");
+        }
+    }
+}
+
+
+
+class Receivemsg implements Runnable {
+    @Override
+    public void run() {
+        String line;
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(ChatServer.s.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            while((line = in.readLine()) != null) {
+                if(line.equals("Client disconnected")){
+                    System.out.println("> Client: bye");
+                    System.out.println(line);
+                    break;
+                }
+                System.out.println("> Client: " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("Disconnected");
+        }
+    }
+}
